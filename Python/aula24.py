963317665d071de341c2e8b480fc483916654a0d
igorFerreiraB <igorfgb2006@gmail.com>
2023-09-30 09:21:02 -0300

    Create aula24.py

---
+++ Python/aula24.py
@@ -0,0 +1,6 @@
+# Programação síncrono e assíncrono 
+# List comprehension
+
+# síncrono: é aquela que precisa esperar a outra ser terminada para executar uma outra.
+# assíncrona: pelo contrario da síncrona pode ecxutar multiplas tafefas ao mesmo tempo
+
