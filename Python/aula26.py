6edd2ef39d5a0c4549deb71220bcbfbf7f300a8e
igorFerreiraB <igorfgb2006@gmail.com>
2023-10-09 18:39:10 -0300

    up

---
+++ Python/aula26.py
@@ -0,0 +1,20 @@
+# forma comum
+# for item in range(10):
+#     lista.append(item*2)
+
+# list comprehensions
+# lista = [item**2 for item in range(10)]
+
+# print (lista)
+
+# list comprehensions também pode conter condicionais para criar listas ou modificar listas existentes
+
+# forma comum
+# for item in range(10):
+#     if item % 2 == 0:
+#         print (item)
+
+# list comprehensions 
+result = [item for item in range(20) if item % 2 == 0]
+
+print(result)
