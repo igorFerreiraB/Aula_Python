cbaf18294073f8d2f3caeeefdda675d660766115
Igor Ferreira <131207955+igorFerreiraB@users.noreply.github.com>
2023-11-20 18:49:18 -0300

    new classes

---
+++ Python/Poo/aula5.py
@@ -0,0 +1,15 @@
+class Pessoa:
+    ano_atual = 2023
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+    
+pessoa1 = Pessoa("igor", 16)
+pessoa2 = Pessoa("jose", 28)
+
+print(pessoa1.ano_nascimento())
+print(pessoa2.ano_nascimento())
