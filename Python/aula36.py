e735c8bb872d7b9250cee24700871063d46508fa
Igor Ferreira <igorfgb2006@gmail.com>
2024-01-11 14:18:24 -0300

    new classes

---
+++ Python/aula36.py
@@ -0,0 +1,25 @@
+class Escritor:
+    def __init__(self, nome):
+        self.nome = nome
+        self.ferramenta = None
+    
+    @property
+    def ferramenta(self):
+        return self._ferramenta
+    
+    @ferramenta.setter
+    def ferramenta(self, ferramenta):
+        self._ferramenta = ferramenta
+
+class FerramentaDeEscrever:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def escrever(self):
+        return f'{self.nome} está escrevendo'
+    
+escritor = Escritor('Igor')
+caneta = FerramentaDeEscrever('Caneta Bic')
+escritor.ferramenta = caneta
+
+print(caneta.escrever())
